8334558378dee44382b026744daa5dd7d3f46247
Ryousuke Tanno <tanno-r@mm.inf.uec.ac.jp>
2016-11-20 12:36:08 +0900

    Performing Image Recognition with Inception_v3 Network using Metal Performance Shaders Convolutional Neural Network routines

---
+++ MPS/MetalImageRecognition/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//
+//  Created by Ryousuke Tanno on 2016/11/20.
+//  Copyright (c) 2016年 Ryosuke Tanno. All rights reserved.
+//
+
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
+
+
